3e31e025cfa8cfcbf33a7fa76426574077eb961b
filezim <teste0309@gmail.com>
2016-09-05 10:07:04 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+namespace ElXerath
+{
+    using LeagueSharp.Common;
+
+    internal class Program
+    {
+        #region Methods
+
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Xerath.Game_OnGameLoad;
+        }
+
+        #endregion
+    }
+}
